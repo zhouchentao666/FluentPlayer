2abedb066032eb517a7174688238d92b9f993fb3
zhouchentao6 <zhouchentao6@outlook.com>
2026-07-30 14:30:07 +0800

    feat(editor): 将歌曲编辑器改为独立窗口

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import '@bridge/runtime'
 import { createApp } from 'vue'
 import App from './App.vue'
+import EditorApp from './EditorApp.vue'
 import DesktopLyricApp from './DesktopLyricApp.vue'
 import './style.css'
 
 const params = new URLSearchParams(window.location.search)
 let root = App
-if (params.get('desktopLyric') === '1') {
+if (params.get('editor') === '1') {
+  root = EditorApp
+} else if (params.get('desktopLyric') === '1') {
   root = DesktopLyricApp
 }
 
